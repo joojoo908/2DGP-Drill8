dba9d75e3b6d879a04385185a8352892b942a7ad
joojoo908 <looloo8910@gmail.com>
2024-10-29 10:26:34 +0900

    이동

--- boy.py
+++ boy.py
@@ -8,15 +8,18 @@ class Boy:
         #self.name=name
         self.x, self.y = 400, 90
         self.frame = 0
-        self.dir = 0
-        self.action = 3
+        self.dir = 1
+        self.face_dir=0;
+        self.action = 1
         #self.wait_time=0;
         self.image = load_image('animation_sheet.png')
         self.state_machine = StateMachine(self)
         self.state_machine.start(Idle)
         self.state_machine.set_transitions({
-            Idle: {time_out:Sleep},
-            Sleep:{space_down:Idle}})
+            Idle: {time_out:Sleep, right_down:Run ,left_down:Run ,space_down:Idle },
+            Sleep:{space_down:Idle , right_down:Run ,left_down:Run},
+            Run:{right_up:Idle , left_up:Idle , space_down:Run}
+        })
 
     def update(self):
         self.state_machine.update()
@@ -28,20 +31,28 @@ class Boy:
 
     def draw(self):
         self.state_machine.draw()
-        #self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+
+    def fire_ball(self):
+        if self.action%2==0:
+            print('fireball_left')
+        else:
+            print('fireball_right')
 
 class Idle:
     @staticmethod
     def enter(boy,e):
-        boy.action=3
-        boy.dir=0
+
+        if boy.action<2:
+            boy.action+=2
+        #boy.dir=0
         boy.frame=0
         boy.wait_time = get_time()
         pass
         #print('Boy Idle Enter')
     @staticmethod
     def exit(boy,e):
-        #print('Boy Idle Exit')
+        if space_down(e):
+            boy.fire_ball()
         pass
     @staticmethod
     def do(boy):
@@ -55,7 +66,9 @@ class Idle:
 class Sleep:
     @staticmethod
     def enter(boy,e):
-        #boy.frame=0
+        #if start_event(e):
+
+        boy.frame=0
         pass
     @staticmethod
     def exit(boy,e):
@@ -65,9 +78,41 @@ class Sleep:
         boy.frame = (boy.frame+1)%8
     @staticmethod
     def draw(boy):
-        boy.image.clip_composite_draw(
-            boy.frame *100, 300, 100, 100,
-            3.141592/2, # 90도 회전
-            '', # 좌우상하 반전 X
-            boy.x - 25, boy.y - 25, 100, 100
-        )
+        if boy.action ==3:
+            boy.image.clip_composite_draw(
+                boy.frame *100, 300, 100, 100,
+                3.141592/2, # 90도 회전
+                '', # 좌우상하 반전 X
+                boy.x - 25, boy.y - 25, 100, 100
+            )
+        else:
+            boy.image.clip_composite_draw(
+                boy.frame * 100, 200, 100, 100,
+                3.141592/2*3,  # 90도 회전
+                '',  # 좌우상하 반전 X
+                boy.x + 25, boy.y - 25, 100, 100)
+
+class Run:
+    @staticmethod
+    def enter(boy, e):
+        if right_down(e) or left_up(e):
+            boy.dir,boy.action=1,1
+        if left_down(e) or right_up(e):
+            boy.dir,boy.action=-1,0
+
+        pass
+
+    @staticmethod
+    def exit(boy, e):
+        if space_down(e):
+            boy.fire_ball()
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x+=boy.dir*5
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
--- state_machin.py
+++ state_machin.py
@@ -5,6 +5,15 @@ def space_down(e):
 def time_out(e):
     return e[0]=='TIME_OUT'
 
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key==SDLK_RIGHT
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key==SDLK_RIGHT
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key==SDLK_LEFT
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key==SDLK_LEFT
+
 
 class StateMachine:
     def __init__(self,o):
